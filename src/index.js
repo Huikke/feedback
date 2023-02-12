038c94268d850c17c424ce7985bc9b3d86826c18
Lassi Chen <lachen@utu.fi>
2023-02-13 00:21:34 +0200

    t1.7 done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App-1.6'
+import App from './App-1.7'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
